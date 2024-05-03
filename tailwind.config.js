3ff40d9def1e229a0e13ee527a5a3682f7899af3
yosepafrr <yosepadrianafauziramdani@gmail.com>
2024-05-03 17:04:45 +0700

    ip fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          'main-color': '#F4FEFF',
+          'second-color': '#45BDBF'
+        }
+      },
+      screens: {
+        'tablet': {'max': '1024px'},
+        'tablet-sm': {'max': '768px'},
+        'android': {'max': '576px'},
+        'android-sm': {'max': '414px'},
+        'iphone': {'max': '355px'}
+      }
+
+    },
   },
   plugins: [],
 }
